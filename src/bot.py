2b865f5213cbd9ff02da74854c5750096c558e97
Vladislav Yaroshchuk <Vladislav.Yaroshchuk@jetbrains.com>
2021-12-06 23:57:40 +0300

    send new year message only on jan 1st

--- src/bot.py
+++ src/bot.py
@@ -46,7 +46,8 @@ class CleaningReminder:
     def add_remind_time(self, remind_time, remind_day_of_month):
         # 21:01 UTC == 00:01 Moscow
         schedule.every().day.at('21:01').do(
-            lambda: self.bot.send_message(chat_id, "Happy New Year!") if date.today().day == 1 else lambda: None)
+            lambda: self.bot.send_message(chat_id,
+                                          "Happy New Year!") if date.today().day == 1 and date.today().month == 1 else lambda: None)
 
         schedule.every().day.at(remind_time).do(
             lambda: self.__clean_reminder() if date.today().day == remind_day_of_month else lambda: None)
