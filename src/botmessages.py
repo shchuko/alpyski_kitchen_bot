32cdc11d0d19a91b0d71d81682045d480e9f70b8
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2022-08-24 11:35:02 +0300

    Update help: remove faq commands

--- src/botmessages.py
+++ src/botmessages.py
@@ -7,8 +7,6 @@ hlp = '''Good day!
 
 /help - show this help / показать это сообщение
 /links - useful links / полезные ссылки
-/faq_ru - ответы на распространенные вопросы
-/faq_en - answers for common questions
 /who_clean - today's cleaning schedule / кто убирается сегодня
 
 For the kitchen cleaning schedule check pinned message and #cleaning hashtag
